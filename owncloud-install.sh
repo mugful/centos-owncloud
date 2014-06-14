23d668f5f0a9db8aa41a31fd289466bf3146ee04
Jiří Stránský <jistr@jistr.com>
2014-06-14 19:23:19 +0200

    Fix install script

--- owncloud-install.sh
+++ owncloud-install.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-set -exo pipefail
+set -euxo pipefail
+
+yum -y install wget
 
 rpm -Uvh http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 
 wget -O /etc/yum.repos.d/isv:ownCloud:community.repo http://download.opensuse.org/repositories/isv:/ownCloud:/community/CentOS_CentOS-6/isv:ownCloud:community.repo
 
-yum -y install mod_ssl mysql-server owncloud supervisor
+yum -y install httpd mod_ssl mysql-server owncloud supervisor
+
+# CentOS image comes cleaned of locales, reinstall them
+yum -y reinstall glibc-common
 
 # not present in centos image, mysql wants it to exist
 touch /etc/sysconfig/network
